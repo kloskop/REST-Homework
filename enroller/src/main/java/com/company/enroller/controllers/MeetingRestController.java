e423bbc0e0f89449ffaca7a255f7b35cc76cb2fc
Paweł Kłósko <kloskopawel@gmail.com>
2018-05-18 19:37:00 +0200

    Initial Commit

---
+++ enroller/src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -0,0 +1,63 @@
+package com.company.enroller.controllers;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.company.enroller.model.Meeting;
+import com.company.enroller.model.Participant;
+import com.company.enroller.persistence.MeetingService;
+import com.company.enroller.persistence.ParticipantService;
+
+@RestController
+@RequestMapping("/meetings")
+public class MeetingRestController {
+
+	@Autowired
+	MeetingService meetingService;
+
+	@Autowired
+	ParticipantService participantService;
+
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public ResponseEntity<?> getMeetings() {
+
+		Collection<Meeting> meetings = meetingService.getAll();
+		return new ResponseEntity<Collection<Meeting>>(meetings, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> getMeeting(@PathVariable("id") Long id) {
+		Meeting meeting = meetingService.findById(id);
+		if (meeting == null) {
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Meeting>(meeting, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public ResponseEntity<?> addMeeting(@RequestBody Meeting meeting) {
+		if (meetingService.findById(meeting.getId()) != null) {
+			return new ResponseEntity("Cannot create meeting: " + meeting.getId(),
+					HttpStatus.CONFLICT);
+		}
+		meetingService.addMeeting(meeting);
+		return new ResponseEntity<Meeting>(meeting, HttpStatus.OK);
+
+	}
+
+	
+
+	
+}
---
+++ enroller/src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -0,0 +1,38 @@
+package com.company.enroller.persistence;
+
+import java.util.Collection;
+
+import org.hibernate.Query;
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Component;
+
+import com.company.enroller.model.Meeting;
+import com.company.enroller.model.Participant;
+
+@Component("meetingService")
+public class MeetingService {
+
+	DatabaseConnector connector;
+
+	public MeetingService() {
+		connector = DatabaseConnector.getInstance();
+	}
+
+	public Collection<Meeting> getAll() {
+		String hql = "FROM Meeting";
+		Query query = connector.getSession().createQuery(hql);
+		return query.list();
+	}
+
+	public Meeting findById(Long id) {
+
+		return (Meeting) connector.getSession().get(Meeting.class, id);
+	}
+
+	public void addMeeting(Meeting meeting) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().save(meeting);
+		transaction.commit();
+	}
+
+}
